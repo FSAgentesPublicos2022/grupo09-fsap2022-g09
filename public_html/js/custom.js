25122bda5279f6c734d39eb20042a04e13a0a98c
dac27 <daniel.chungara@gmail.com>
2022-06-15 01:02:11 -0300

    Add files via upload
    se agregaron validaciones en registro y login

--- public_html/js/custom.js
+++ public_html/js/custom.js
@@ -1,15 +1,20 @@
 
+/*
 document.getElementById("btn_iniciarSesion").onclick = function () {
   alert("Hola usuario!");
 };
+*/
 
 
 
 
+
+/*
 function prompter() {
   var reply = prompt("Hola ¿cuál es tu nombre?", "")
   alert ( "Es un gusto tenerte aqui " + reply + "!")
   }
+*/
 
 
 
@@ -17,10 +22,10 @@ function prompter() {
 
 
 function confirmacion() {
-	var pregunta = confirm("¿Deseas visitar la pagina principal?")
+	var pregunta = confirm("¿Deseas ingresar a Opcoin?")
 	if (pregunta){
 		alert("Te envio ahi rapidamente")
-		window.location = "https://www.google.com/";
+		window.location = "http://127.0.0.1:5500/login.html";
 	}
 	else{
 		alert("Quizas en otro momento...\n Gracias de todas formas")

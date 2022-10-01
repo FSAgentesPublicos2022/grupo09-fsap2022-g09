b0e87722ba1c5f24a84907fb12620cc09ba366fe
dac27 <daniel.chungara@gmail.com>
2022-10-01 17:05:32 -0300

    desarrollo

---
+++ BEopcoin/BEopcoin/Models/Rol.cs
@@ -0,0 +1,31 @@
+namespace BEopcoin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Rol")]
+    public partial class Rol
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Rol()
+        {
+            Usuario = new HashSet<Usuario>();
+        }
+
+        [Key]
+        public int ID_Rol { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string NombreRol { get; set; }
+
+        [StringLength(50)]
+        public string Descripcion { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Usuario> Usuario { get; set; }
+    }
+}
